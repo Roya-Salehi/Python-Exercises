59f783b22112b938ce8464be6d544f15d56b19ff
Roya Salehi <69385924+Roya-Salehi@users.noreply.github.com>
2022-08-01 19:07:39 +0430

    Improvement in exercise 05.

--- Solutions/05ListOverlap.py
+++ Solutions/05ListOverlap.py
@@ -4,4 +4,5 @@ b = [random.randrange(50) for i in range(random.randint(0, 20))]
 a.sort()
 b.sort()
 print(f"a = {a}\nb = {b}\n")
+print(list(dict.fromkeys(n for n in a if n in b)))
 print(list(set([n for n in a if n in b])))

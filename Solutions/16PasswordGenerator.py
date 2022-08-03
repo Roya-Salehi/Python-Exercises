351fc4b183d5a59cf7426b1ca69ec66ad971fd3b
Roya Salehi <69385924+Roya-Salehi@users.noreply.github.com>
2022-08-03 23:04:31 +0430

    Including run-time code in a main method in the solution for exercise 16.

--- Solutions/16PasswordGenerator.py
+++ Solutions/16PasswordGenerator.py
@@ -2,27 +2,26 @@ import random
 import string
 
 
-def pass_gen_simple_solution(lenght):
-    return "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=lenght))
+def main():
+    def pass_gen_simple_solution(lenght):
+        return "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=lenght))
 
+    def pass_gen_short_solution(lenght):
+        return "".join(random.choices(string.printable[:94], k=lenght))
 
-def pass_gen_short_solution(lenght):
-    return "".join(random.choices(string.printable[:94], k=lenght))
+    listOfRandomWords = ["Bumfuzzle", "Cattywampus", "Gardyloo", "Taradiddle", "Snickersnee",
+                         "Widdershins", "Collywobbles", "Gubbins", "Abibliophobia", "Bumbershoot"]
 
+    strenght = input(
+        "Enter 's' for a strong password, or 'w' for a weak password: ").lower()
 
-listOfRandomWords = ["Bumfuzzle", "Cattywampus", "Gardyloo", "Taradiddle", "Snickersnee",
-                     "Widdershins", "Collywobbles", "Gubbins", "Abibliophobia", "Bumbershoot"]
+    if strenght == 's':
+        lenght = int(input("Enter the lenght of the password: "))
+        passw1 = pass_gen_simple_solution(lenght)
+        passw2 = pass_gen_short_solution(lenght)
+        print(passw1, passw2, sep="\n")
 
-strenght = input(
-    "Enter 's' for a strong password, or 'w' for a weak password: ").lower()
-
-if strenght == 's':
-    lenght = int(input("Enter the lenght of the password: "))
-    passw1 = pass_gen_simple_solution(lenght)
-    passw2 = pass_gen_short_solution(lenght)
-    print(passw1, passw2, sep="\n")
-
-else:
-    def weakpass(): return "".join(random.choices(
-        listOfRandomWords, k=random.randint(1, 2)))
-    print(weakpass())
+    else:
+        def weakpass(): return "".join(random.choices(
+            listOfRandomWords, k=random.randint(1, 2)))
+        print(weakpass())
